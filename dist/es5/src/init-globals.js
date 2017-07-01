860bc22b2ba07e4669702e5ef068a8b00161cc79
danielweck <daniel.weck@gmail.com>
2017-07-02 00:59:51 +0100

    add new dist

--- dist/es5/src/init-globals.js
+++ dist/es5/src/init-globals.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var metadata_collection_1 = require("./models/metadata-collection");
+var metadata_collection_json_converter_1 = require("./models/metadata-collection-json-converter");
 var metadata_contributor_1 = require("./models/metadata-contributor");
 var metadata_contributor_json_converter_1 = require("./models/metadata-contributor-json-converter");
+var opds2_collection_1 = require("./opds/opds2/opds2-collection");
+var opds2_collection_json_converter_1 = require("./opds/opds2/opds2-collection-json-converter");
 var ta_json_date_converter_1 = require("./_utils/ta-json-date-converter");
 var xml_js_mapper_1 = require("./_utils/xml-js-mapper");
 var ta_json_1 = require("ta-json");
@@ -9,6 +13,8 @@ function initGlobals() {
     ta_json_1.propertyConverters.set(Buffer, new ta_json_1.BufferConverter());
     ta_json_1.propertyConverters.set(Date, new ta_json_date_converter_1.JsonDateConverter());
     ta_json_1.propertyConverters.set(metadata_contributor_1.Contributor, new metadata_contributor_json_converter_1.JsonContributorConverter());
+    ta_json_1.propertyConverters.set(metadata_collection_1.Collection, new metadata_collection_json_converter_1.JsonCollectionConverter());
+    ta_json_1.propertyConverters.set(opds2_collection_1.OPDSCollection, new opds2_collection_json_converter_1.JsonOPDSCollectionConverter());
     xml_js_mapper_1.propertyConverters.set(Buffer, new xml_js_mapper_1.BufferConverter());
     xml_js_mapper_1.propertyConverters.set(Date, new xml_js_mapper_1.DateConverter());
 }
---
+++ dist/es8-es2017/test/test-JSON-Title.js
@@ -0,0 +1,104 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const metadata_1 = require("../src/models/metadata");
+const opds2_publicationMetadata_1 = require("../src/opds/opds2/opds2-publicationMetadata");
+const ava_1 = require("ava");
+const ta_json_1 = require("ta-json");
+const init_globals_1 = require("../src/init-globals");
+const helpers_1 = require("./helpers");
+init_globals_1.initGlobals();
+const titleStr1 = "str1";
+const titleStr2 = "str2";
+const titleLang1 = "lang1";
+const titleLang2 = "lang2";
+const titleLangStr1 = {};
+titleLangStr1[titleLang1] = titleStr1;
+titleLangStr1[titleLang2] = titleStr2;
+const titleLangStr2 = {};
+titleLangStr2[titleLang1] = titleStr2;
+titleLangStr2[titleLang2] = titleStr1;
+ava_1.test("JSON SERIALIZE: OPDSPublicationMetadata.Title => string", (t) => {
+    const md = new opds2_publicationMetadata_1.OPDSPublicationMetadata();
+    md.Title = titleStr1;
+    helpers_1.inspect(md);
+    const json = ta_json_1.JSON.serialize(md);
+    helpers_1.logJSON(json);
+    helpers_1.checkType_String(t, json.title);
+    t.is(json.title, titleStr1);
+});
+ava_1.test("JSON SERIALIZE: OPDSPublicationMetadata.Title => string-lang", (t) => {
+    const md = new opds2_publicationMetadata_1.OPDSPublicationMetadata();
+    md.Title = titleLangStr1;
+    helpers_1.inspect(md);
+    const json = ta_json_1.JSON.serialize(md);
+    helpers_1.logJSON(json);
+    helpers_1.checkType_Object(t, json.title);
+    helpers_1.checkType_String(t, json.title[titleLang1]);
+    t.is(json.title[titleLang1], titleStr1);
+    helpers_1.checkType_String(t, json.title[titleLang2]);
+    t.is(json.title[titleLang2], titleStr2);
+});
+ava_1.test("JSON DESERIALIZE: OPDSPublicationMetadata.Title => string", (t) => {
+    const json = {};
+    json.title = titleStr1;
+    helpers_1.logJSON(json);
+    const md = ta_json_1.JSON.deserialize(json, opds2_publicationMetadata_1.OPDSPublicationMetadata);
+    helpers_1.inspect(md);
+    helpers_1.checkType_String(t, md.Title);
+    t.is(md.Title, titleStr1);
+});
+ava_1.test("JSON DESERIALIZE: OPDSPublicationMetadata.Title => string-lang", (t) => {
+    const json = {};
+    json.title = titleLangStr1;
+    helpers_1.logJSON(json);
+    const md = ta_json_1.JSON.deserialize(json, opds2_publicationMetadata_1.OPDSPublicationMetadata);
+    helpers_1.inspect(md);
+    helpers_1.checkType_Object(t, md.Title);
+    helpers_1.checkType_String(t, md.Title[titleLang1]);
+    t.is(md.Title[titleLang1], titleStr1);
+    helpers_1.checkType_String(t, md.Title[titleLang2]);
+    t.is(md.Title[titleLang2], titleStr2);
+});
+ava_1.test("JSON SERIALIZE: Metadata.Title => string", (t) => {
+    const md = new metadata_1.Metadata();
+    md.Title = titleStr1;
+    helpers_1.inspect(md);
+    const json = ta_json_1.JSON.serialize(md);
+    helpers_1.logJSON(json);
+    helpers_1.checkType_String(t, json.title);
+    t.is(json.title, titleStr1);
+});
+ava_1.test("JSON SERIALIZE: Metadata.Title => string-lang", (t) => {
+    const md = new metadata_1.Metadata();
+    md.Title = titleLangStr1;
+    helpers_1.inspect(md);
+    const json = ta_json_1.JSON.serialize(md);
+    helpers_1.logJSON(json);
+    helpers_1.checkType_Object(t, json.title);
+    helpers_1.checkType_String(t, json.title[titleLang1]);
+    t.is(json.title[titleLang1], titleStr1);
+    helpers_1.checkType_String(t, json.title[titleLang2]);
+    t.is(json.title[titleLang2], titleStr2);
+});
+ava_1.test("JSON DESERIALIZE: Metadata.Title => string", (t) => {
+    const json = {};
+    json.title = titleStr1;
+    helpers_1.logJSON(json);
+    const md = ta_json_1.JSON.deserialize(json, metadata_1.Metadata);
+    helpers_1.inspect(md);
+    helpers_1.checkType_String(t, md.Title);
+    t.is(md.Title, titleStr1);
+});
+ava_1.test("JSON DESERIALIZE: Metadata.Title => string-lang", (t) => {
+    const json = {};
+    json.title = titleLangStr1;
+    helpers_1.logJSON(json);
+    const md = ta_json_1.JSON.deserialize(json, metadata_1.Metadata);
+    helpers_1.inspect(md);
+    helpers_1.checkType_Object(t, md.Title);
+    helpers_1.checkType_String(t, md.Title[titleLang1]);
+    t.is(md.Title[titleLang1], titleStr1);
+    helpers_1.checkType_String(t, md.Title[titleLang2]);
+    t.is(md.Title[titleLang2], titleStr2);
+});
+//# sourceMappingURL=test-JSON-Title.js.map
